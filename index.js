6cea0024167f149d9db286ac378980254d8df872
taarannn <165926771+taarannn@users.noreply.github.com>
2024-04-03 22:34:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const http =require("http");
 const fs = require("fs");
+const url = require("url");
+const { stripVTControlCharacters } = require("util");
 const myserver = http.createServer( (req , res)=> {
- const log ='${Date.now()}: ${req.url} New Req Received\n'
+ const log = '${Date.now()}: ${request.method} ${req.url} New Req Received\n';
+ const myurl = url.parse(req.url , true);
+ console.log(myurl);
  fs.appendFile("log.text", log,(err , data)=>{
-    switch(req.url){
+    switch(myurl.pathname){
         case"/":
-        res.end("Homepage");
+        if(req.method === 'GET')
+        res.end("Homepage"); 
         break;
         case "/about":
-        res.end("I am taran");
+            const username = myurl.query.myname;
+        res.end('hii', username);
         break;
+
+        case '/signup':
+            if(req.method ==='GET')res.end("this is a sign up");
+            else if (req.method === "POST"){
+                //db query
+                res.end("sucess");
+            }
         default:
             res.end("404 NOt Found");
     }
