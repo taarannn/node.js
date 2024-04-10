feafe0f56ed8a2b79aa15ab0bd446d4543bf1481
taarannn <165926771+taarannn@users.noreply.github.com>
2024-04-11 00:01:28 +0530

    module.export

---
+++ module.js
@@ -0,0 +1,14 @@
+ const addfun = require("./add");
+
+
+ 
+const sum = addfun(1,2);
+const sum2 = addfun(2,3);
+
+ console.log(sum);
+ console.log(sum2);
+
+ const math = require("./add");
+ console.log(math.addFn(2,3));
+ console.log(math.subtract(2,3));
+
